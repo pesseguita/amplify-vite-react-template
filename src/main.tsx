66d7265594b7b070743c2acebbb118504491ef4d
pesseguita <paula.pamplona.martins@gmail.com>
2025-11-30 11:38:17 +0100

    feat: copy template repo

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 
+// Configure Amplify with outputs
 Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
